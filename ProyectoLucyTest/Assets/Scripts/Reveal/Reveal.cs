6aa547f5685375ed53422534d01a4eadaa9b4112
ieKatt <98968033+ieKatt@users.noreply.github.com>
2022-02-24 18:25:39 +0100

    24/02

---
+++ ProyectoLucyTest/Assets/Scripts/Reveal/Reveal.cs
@@ -0,0 +1,20 @@
+﻿//Shady
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Reveal : MonoBehaviour
+{
+    [SerializeField] Material Mat;
+    [SerializeField] Light SpotLight;
+	
+	void FixedUpdate ()
+    {
+    	if(Mat && SpotLight)
+    	{
+    		Mat.SetVector("MyLightPosition",  SpotLight.transform.position);
+        	Mat.SetVector("MyLightDirection", -SpotLight.transform.forward );
+        	Mat.SetFloat ("MyLightAngle", SpotLight.spotAngle);
+    	}
+        
+    }
+}
